0fbc0391af17097355d2a6fa0f16860ccc37bff2
manjularkm90-max <manjularkm90@gmail.com>
2026-02-11 21:41:52 +0530

    Add files via upload

---
+++ fun1.c
@@ -0,0 +1,22 @@
+//maximum of  two numbers
+#include<stdio.h>
+int big(int a,int b){
+    if(a>b){
+        printf("%d is greater than %d",a,b);
+    }
+    else if(b>a){
+        printf("%d is greater than %d",b,a);
+    }
+    else{
+        printf("both are equal");
+    }
+
+    return 0;
+}
+int main(){
+    int a,b;
+    scanf("%d%d",&a,&b);
+    big(a,b);
+    return 0;
+}
+
